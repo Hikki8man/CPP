9447d698aa469799bb693f9cd25a25fe1afc11f0
hikki <johan.chevet@hotmail.fr>
2022-02-12 16:59:15 +0100

    cpasouf

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -9,10 +9,20 @@ template< typename T >
 class MutantStack : public std::stack<T> {
 
 	public :
+		typedef typename MutantStack::container_type::iterator iterator;
+
 		MutantStack() : std::stack<T>() {};
-		MutantStack(MutantStack const & src) { static_cast<void>(src); };
+		MutantStack(MutantStack const & src) { *this = src; };
 		~MutantStack() {};
 
+		using std::stack<T>::operator=;
+		
+		iterator begin() {
+			return this->c.begin();
+		}
+		iterator end() {
+			return this->c.end();
+		}
 };
 
 #endif
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -1,18 +1,58 @@
 # include "MutantStack.hpp"
+#include <list>
 
 int main() {
 
 	MutantStack<int> m;
 	std::stack<int> s;
+	MutantStack<int> mcpy(m);
+	// std::vector<int> c;
 
+
+	m.push(4);
+	m.push(1);
 	m.push(12);
 	m.push(1);
-	s.push(12);
-	s.push(1);
+	
+	// Stack<int>::value_type r;
+	// MutantStack<int>::iterator
 
-	std::cout << "stack: " << s.top() << std::endl;
+	// std::cout << "stack: " << s.top() << std::endl;
 	std::cout << "stack: " << m.top() << std::endl;
 
+	
+	MutantStack<int>::iterator ite = m.end();
+
+	for (MutantStack<int>::iterator it = m.begin();  it != ite; it++)
+		std::cout << *it << std::endl;
+
+
+	MutantStack<int>::iterator itee = mcpy.end();
+
+	for (MutantStack<int>::iterator it = mcpy.begin();  it != itee; it++)
+		std::cout << *it << std::endl;
+
+	// std::list<int> m;
+	// std::stack<int> s;
+	// // std::vector<int> c;
+
+
+	// m.push_back(4);
+	// m.push_back(1);
+	// m.push_back(12);
+	// m.push_back(1);
+	
+	// // Stack<int>::value_type r;
+	// // MutantStack<int>::iterator
+
+	// // std::cout << "stack: " << s.top() << std::endl;
+	// // std::cout << "stack: " << m.top() << std::endl;
+
+	
+	// std::list<int>::iterator ite = m.end();
+
+	// for (std::list<int>::iterator it = m.begin();  it != ite; it++)
+	// 	std::cout << *it << std::endl;
 
 	return 0;
 }
